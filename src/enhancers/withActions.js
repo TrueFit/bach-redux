8cbb7f63b0d5864ccb0e033e7aead9cb5bef0150
Josh Gretz <jgretz@truefit.io>
2019-05-23 18:01:16 -0400

    update for IE 11 support

--- src/enhancers/withActions.js
+++ src/enhancers/withActions.js
@@ -1,20 +1,24 @@
-import {generateConditionCode} from '@truefit/bach';
+import {generateConditionCode, generateAssignments} from '@truefit/bach';
 import {useActions} from '../hooks';
 import {USE_ACTIONS} from '../util/constants';
 
 export default (actions, conditions) => ({generateNewVariable}) => {
   const actionKeys = Object.keys(actions);
   const actionRef = generateNewVariable();
+  const valueRef = generateNewVariable();
 
-  const actionVars = actionKeys.join(',');
   const conditionCode = generateConditionCode(conditions);
+  const assignments = generateAssignments(actionKeys, valueRef);
 
   return {
     dependencies: {
       [USE_ACTIONS]: useActions,
       [actionRef]: actions,
     },
-    initialize: `const {${actionVars}} = ${USE_ACTIONS}(${actionRef}, ${conditionCode});`,
+    initialize: `
+      const ${valueRef} = ${USE_ACTIONS}(${actionRef}, ${conditionCode});
+      ${assignments}
+    `,
     props: actionKeys,
   };
 };
